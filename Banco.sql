0e027a7ab5df808c9484020dec3a045630d95a7d
Camila Arena <camiarena@yahoo.com.ar>
2021-09-16 17:15:23 -0300

    Update Banco.sql

--- Banco.sql
+++ Banco.sql
@@ -1,8 +1,8 @@
 # Creo de la Base de Datos
-DROP DATABASE banco; /*revisar*/
+DROP DATABASE banco; 
 CREATE DATABASE banco;
 
-# selecciono la base de datos sobre la cual voy a hacer modificaciones
+# selecciono la base de datos
 USE banco;
 
 #-------------------------------------------------------------------------
@@ -75,13 +75,13 @@ CREATE TABLE Cliente(
 ) ENGINE=InnoDB;
 
 
-CREATE TABLE PlazoFijo(
+CREATE TABLE Plazo_Fijo(
     nro_plazo BIGINT NOT NULL CHECK (nro_plazo > 0 AND nro_plazo <= 99999999),
     capital FLOAT NOT NULL CHECK (capital > 0),
     fecha_inicio DATE NOT NULL,
     fecha_fin DATE NOT NULL,
-    tasa_interes DECIMAL (4,2) NOT NULL,
-    interes DECIMAL(6,2) NOT NULL,
+    tasa_interes DECIMAL (4,2) unsigned NOT NULL,
+    interes DECIMAL(6,2) unsigned NOT NULL,
     nro_suc INT NOT NULL, 
 
    CONSTRAINT pk_PlazoFijo 
@@ -96,9 +96,10 @@ CREATE TABLE PlazoFijo(
 
 CREATE TABLE Tasa_Plazo_Fijo(
     periodo SMALLINT NOT NULL CHECK (periodo > 0 AND periodo <= 999),
-    monto_inf UNSIGNED DECIMAL(9,2) NOT NULL,
-    monto_sup UNSIGNED DECIMAL(9,2) NOT NULL,
-    tasa DUNSIGNED DECIMAL(4,2) NOT NULL,
+    monto_inf  DECIMAL(9,2) unsigned NOT NULL,
+    monto_sup  DECIMAL(9,2) unsigned NOT NULL,
+    tasa  DECIMAL(4,2) unsigned CHECK(tasa>0) NOT NULL,
+    
 
     CONSTRAINT pk_Tasa_Plazo_Fijo
 	    PRIMARY KEY (periodo, monto_inf, monto_sup)
@@ -108,7 +109,7 @@ CREATE TABLE Tasa_Plazo_Fijo(
 
 CREATE TABLE Plazo_Cliente(
 
-	nro_plazo INT NOT NULL,
+	nro_plazo BIGINT NOT NULL,
 	nro_cliente INT NOT NULL,
 	
 	CONSTRAINT pk_Plazo_Cliente
@@ -130,10 +131,10 @@ CREATE TABLE Prestamo(
     nro_prestamo BIGINT NOT NULL CHECK (nro_prestamo > 0 AND nro_prestamo <= 99999999),   
     fecha DATE NOT NULL,
     cant_meses SMALLINT NOT NULL CHECK (cant_meses > 0 AND cant_meses <= 99),
-    monto UNSIGNED DECIMAL(9,2) NOT NULL, 
-    tasa_interes UNSIGNED DECIMAL(4,2) NOT NULL, 
-    interes UNSIGNED DECIMAL(6,2) NOT NULL, 
-    valor_cuota UNSIGNED DECIMAL(7,2) NOT NULL, 
+    monto  DECIMAL(9,2) unsigned NOT NULL, 
+    tasa_interes  DECIMAL(4,2) unsigned NOT NULL, 
+    interes  DECIMAL(6,2) unsigned NOT NULL, 
+    valor_cuota  DECIMAL(7,2) unsigned NOT NULL, 
     legajo INT NOT NULL,
     nro_cliente INT NOT NULL,
 
@@ -152,7 +153,7 @@ CREATE TABLE Prestamo(
 
 
 CREATE TABLE Pago(
-    nro_prestamo INT NOT NULL,
+    nro_prestamo BIGINT NOT NULL,
     nro_pago SMALLINT NOT NULL CHECK (nro_pago > 0 AND nro_pago <= 99),
     fecha_venc DATE NOT NULL,
     fecha_pago DATE NOT NULL,
@@ -169,9 +170,9 @@ CREATE TABLE Pago(
 
 CREATE TABLE Tasa_Prestamo(
 	periodo SMALLINT NOT NULL CHECK (periodo > 0 AND periodo <= 999),
-	monto_inf UNSIGNED DECIMAL(9,2) NOT NULL,
-	monto_sup UNSIGNED DECIMAL(9,2) NOT NULL,
-	tasa UNSIGNED DECIMAL(4,2) NOT NULL,
+	monto_inf  DECIMAL(9,2) unsigned NOT NULL,
+	monto_sup  DECIMAL(9,2) unsigned NOT NULL,
+	tasa  DECIMAL(4,2) unsigned NOT NULL,
 	
 	CONSTRAINT pk_Tasa_Prestamo
 	PRIMARY KEY (periodo, monto_inf, monto_sup)
@@ -182,7 +183,7 @@ CREATE TABLE Tasa_Prestamo(
 CREATE TABLE Caja_Ahorro(
 	nro_ca INT NOT NULL CHECK (nro_ca > 0 AND nro_ca <= 99999999),
 	CBU BIGINT NOT NULL CHECK (CBU > 99999999999999999 AND CBU <= 999999999999999999),
-	saldo UNSIGNED DECIMAL(10,2) NOT NULL,
+	saldo DECIMAL(10,2) unsigned NOT NULL,
 	
 	CONSTRAINT pk_Caja_Ahorro
 	PRIMARY KEY (nro_ca)
@@ -208,8 +209,8 @@ CREATE TABLE Cliente_CA (
 
 CREATE TABLE Tarjeta(
 	nro_tarjeta BIGINT NOT NULL CHECK(nro_tarjeta > 0 AND nro_tarjeta <= 9999999999999999), 
-	MD5(PIN INT NOT NULL), 
-	MD5(CVT INT NOT NULL), 
+	PIN INT NOT NULL, 
+	CVT INT NOT NULL, 
 	nro_cliente INT NOT NULL, 
 	nro_ca INT NOT NULL,
 	fecha_venc DATE NOT NULL,	
@@ -240,7 +241,7 @@ CREATE TABLE Caja (
 CREATE TABLE ATM(
 	cod_caja INT NOT NULL, 
 	cod_postal INT NOT NULL,
-	direccion VARCHAR NOT NULL,
+	direccion VARCHAR(45) NOT NULL,
 	
 	CONSTRAINT pk_ATM
 	PRIMARY KEY (cod_caja),
@@ -280,7 +281,7 @@ CREATE TABLE Transaccion(
 	nro_trans INT NOT NULL CHECK(nro_trans > 0 AND nro_trans <= 9999999999), 
 	fecha DATE NOT NULL, 
 	hora TIME NOT NULL, 
-	monto UNSIGNED DECIMAL(9,2) NOT NULL,
+	monto  DECIMAL(9,2) unsigned NOT NULL,
 	
 	CONSTRAINT pk_Transaccion
 	PRIMARY KEY (nro_trans)
@@ -309,7 +310,7 @@ CREATE TABLE Transaccion_por_caja(
 
 CREATE TABLE Debito(
 	nro_trans INT NOT NULL, 	
-	descripcion VARCHAR NOT NULL, 
+	descripcion VARCHAR(45) NOT NULL, 
 	nro_cliente INT NOT NULL,
 	nro_ca INT NOT NULL,
 	
@@ -352,7 +353,7 @@ CREATE TABLE Deposito(
 
 
 CREATE TABLE Extraccion(
-	nro_trans BIGINT NOT NULL, 
+	nro_trans INT NOT NULL, 
 	nro_cliente INT NOT NULL,
 	nro_ca INT NOT NULL,
 		
@@ -394,12 +395,12 @@ CREATE TABLE Transferencia(
 	
 	
 	/*no se si referencia a la llave de la relacion Cliente_CA o si directamente va a Caja_de_ahorro*/
-	CONSTRAINT FK_Deposito_CA
+	CONSTRAINT FK_Transferencia_Cliente_CA_Origen
 	FOREIGN KEY (origen_nroca) REFERENCES Cliente_CA (nro_ca)
 		ON DELETE RESTRICT ON UPDATE CASCADE, 
 	
-	CONSTRAINT FK_Deposito_CA
-	FOREIGN KEY (destino_nroca) REFERENCES Cliente_CA (nro_ca)
+	CONSTRAINT FK_Transferencia_CA_Destino
+	FOREIGN KEY (destino_nroca) REFERENCES Caja_Ahorro (nro_ca)
 		ON DELETE RESTRICT ON UPDATE CASCADE	
 		
 
@@ -420,7 +421,7 @@ CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin';
 
 GRANT ALL PRIVILEGES ON banco.* TO 'admin'@'localhost' WITH GRANT OPTION;
 
-#------------- Usuario: empleado -------------------
+DROP USER admin@localhost;
 
 /* Este usuario estara destinado a permitir el acceso de la aplicacion de administracion
 que utilizan los empleados del banco para administrar los clientes, prestamos, cajas de ahorro y
@@ -437,15 +438,16 @@ empleado. Importante: Recuerde eliminar el usuario vacıo (drop user ’’@loca
 poder conectarse con el usuario empleado desde localhost. */
 # 
 
+DROP USER empleado;
 CREATE USER 'empleado'@'%' IDENTIFIED BY 'empleado';
 
-drop user ’’@localhost /*ESTA BIEN ASI? xd*/
+/*DROP USER ''@localhost;*/
 
 /* Consultas que puede hacer el empleado: */
 GRANT SELECT ON banco.Empleado TO 'empleado'@'%';
 GRANT SELECT ON banco.Sucursal TO 'empleado'@'%';
 GRANT SELECT ON banco.Tasa_Plazo_Fijo TO 'empleado'@'%';
-GRANT SELECT ON banco.Tasa_Prestamo TO 'empleado'@'%';
+GRANT SELECT ON banco.Tasa_Prestamo TO 'empleado'@'%'; 
 
 /*Consultas e ingreso de datos que puede hacer el empelado:*/
 GRANT SELECT, INSERT ON banco.Prestamo TO 'empleado'@'%';
@@ -462,7 +464,7 @@ GRANT SELECT, INSERT, UPDATE ON banco.Pago TO 'empleado'@'%';
 
 #------------- Usuario: atm -------------------
 
-#
+/*
 atm: Este usuario esta destinado a permitir el acceso de los ATM, para que los clientes puedan
 consultar el estado de sus cajas de ahorro y realizar transacciones. Con el objetivo de ocultar
 la estructura de la base de datos, el usuario atm tendra una vision restringida de la misma que
@@ -488,9 +490,19 @@ poder controlar el ingreso de los clientes a los ATM y permitir que cambien el P
 
 Dado que los cajeros automaticos se encuentran distribuidos en diferentes ciudades, este usuario
 debera poder conectarse desde cualquier dominio. El password de este usuario debera ser atm.
-#
+*/
 
 CREATE USER 'atm'@'%' IDENTIFIED BY 'atm';
+DROP USER atm;
 
 GRANT SELECT ON banco.Transaccion_por_caja TO 'atm'@'%';
 GRANT SELECT, UPDATE ON banco.Tarjeta TO 'atm'@'%';
+
+
+
+
+
+
+
+
+
